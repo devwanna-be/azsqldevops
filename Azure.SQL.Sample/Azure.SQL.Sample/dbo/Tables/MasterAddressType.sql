19ae338a14034c1c6e8a85da3a4c942033fed379
Developer Wannabe <triyanto.wulung@gmail.com>
2020-10-04 20:32:36 +0700

    initial

---
+++ Azure.SQL.Sample/Azure.SQL.Sample/dbo/Tables/MasterAddressType.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[MasterAddressType]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [AddressTypeCode] VARCHAR(25) NOT NULL, 
+    [AddressTypeName] VARCHAR(50) NOT NULL, 
+    [CreatedAt] DATETIME NOT NULL, 
+    [CreatedBy] VARCHAR(50) NOT NULL, 
+    [UpdatedAt] DATETIME NULL, 
+    [UpdatedBy] VARCHAR(50) NULL, 
+    [DeletedAt] NCHAR(10) NULL, 
+    [DeletedBy] VARCHAR(50) NULL
+)
